68c67aa04c3cbff926f9dfa2e56db158a9d7f14e
the-hby <3206391486@qq.com>
2023-07-25 00:52:46 +0800

    forth commit

---
+++ chapter14/flexmemb.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct flex{
+    size_t count;
+    double average;
+    double scores[];
+};
+void showFlex(const struct flex*p);
+int main(void)
+{
+    struct flex*pf1,*pf2;
+    int n=5;
+    int i;
+    int tot=0;
+    pf1=malloc(sizeof(struct flex)+n*sizeof(double));
+    pf1->count=n;
+    for(i=0;i<n;i++)
+    {
+        pf1->scores[i]=20.0-i;
+        tot+=pf1->scores[i];
+    }
+    pf1->average=tot/n;
+    showFlex(pf1);
+    free(pf1);
+}
+void showFlex(const struct flex*p)
+{
+    int i;
+    printf("Scores:");
+    for(i=0;i<p->count;i++)
+    {
+        printf("%g",p->scores[i]);
+    }
+    printf("\nAverage: %g\n",p->average);
+}
